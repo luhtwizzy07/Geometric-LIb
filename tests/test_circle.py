71f3acbca8ae1dc83206c46b633857dd19e882f1
Sergey Obukhov <sergei.obuxov.07@mail.ru>
2025-12-26 10:45:58 +0300

    Added new circle tests

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -10,20 +10,20 @@ class CircleTestCase(unittest.TestCase):
 
     def test_zero_perimeter(self):
         res = perimeter(0)
-        self.assertEqual(res,0)
+        self.assertEqual(res, 0)
 
-    def test_valid_area(self):
+    def test_area1(self):
         res = area(2)
         self.assertEqual(res, 12.566370614359172)
 
-    def test_valid_perimeter(self):
+    def test_perimeter1(self):
         res = perimeter(2)
         self.assertEqual(res, 12.566370614359172)
 
-    def test_negative_area(self):
-        res = area(-10)
-        self.assertFalse
+    def test_area2(self):
+        res = area(7.77)
+        self.assertEqual(res, 189.6670591159112)
 
-    def test_negative_perimeter(self):
-        res = perimeter(-2)
-        self.assertFalse
+    def test_perimeter2(self):
+        res = perimeter(7.77)
+        self.assertEqual(res, 48.82034983678538)
